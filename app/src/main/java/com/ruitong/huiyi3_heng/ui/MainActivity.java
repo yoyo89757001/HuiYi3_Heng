19ad61894aac97299b7c951be2cfdf966a22bd22
yoyo89757001 <332663557@qq.com>
2018-06-19 18:14:26 +0800

    Initial commit

---
+++ app/src/main/java/com/ruitong/huiyi3_heng/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.ruitong.huiyi3_heng.ui;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.ruitong.huiyi3_heng.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
